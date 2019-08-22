73ca0c4cbc42bf0022b0886232a6702cd7b7bece
Bohodar <bodik264478@gmail.com>
2019-08-22 23:25:39 +0300

    Rare version of script added

--- src/script.js
+++ src/script.js
@@ -3,6 +3,13 @@
 const form = document.body.querySelector('#main-form');
 function handleSubmit(e) {
   e.preventDefault();
-  console.log(e.target[0].id)
+  const nameValue = e.target.querySelector('#name-field').value;
+  const emailValue = e.target.querySelector('#email-field').value;
+  const phoneValue = e.target.querySelector('#phone-field').value;
+  if (nameValue && emailValue.includes('@') && phoneValue) {
+    window.location.href = 'congrats.html';
+  } else {
+    console.log('So sad :(')
+  }
 }
 form.addEventListener('submit', handleSubmit);
